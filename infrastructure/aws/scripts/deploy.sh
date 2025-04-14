c8541e6defe2c520a00267137f9c03434c0ea86d
Chris Mamon <chrisam1993@gmail.com>
2025-04-14 20:27:08 +1000

    chore: add uv sync and venv activation to deploy and destroy scripts

--- infrastructure/aws/scripts/deploy.sh
+++ infrastructure/aws/scripts/deploy.sh
@@ -1,4 +1,6 @@
 (
     cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit
+    uv sync
+    . ".venv/bin/activate"
     cdk deploy --all --method direct --require-approval never --concurrency 8
 )
--- infrastructure/aws/scripts/destroy.sh
+++ infrastructure/aws/scripts/destroy.sh
@@ -1,4 +1,6 @@
 (
     cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit
+    uv sync
+    . ".venv/bin/activate"
     cdk destroy --all --force
 )
